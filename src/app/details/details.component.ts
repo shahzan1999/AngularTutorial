e720008b99f8e157a66c0b71fe2941f927a65271
mohdshahzan <shahzan1999@gmail.com>
2024-07-16 23:18:19 +0530

    deatils added routing integrated

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {Routes} from '@angular/router';
+import {HomeComponent} from '../home/home.component';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing.service';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +12,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,8 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import {Routes} from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { DetailsComponent } from '../details/details.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
+  imports: [RouterLink,RouterOutlet],
   standalone:true,
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
